7224b52c32d3cc9a78b5f785a9976da1fc2f506b
suhas991 <suhas4497@gmail.com>
2026-03-24 12:33:39 +0530

    added install app feature

---
+++ public/sw.js
@@ -0,0 +1,77 @@
+const CACHE_NAME = 'docs-hub-v1'
+const APP_SHELL = ['/', '/index.html', '/manifest.webmanifest', '/favicon.svg', '/pwa-192x192.png', '/pwa-512x512.png']
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(APP_SHELL)
+    }),
+  )
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key)
+          }
+          return Promise.resolve()
+        }),
+      )
+    }),
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', (event) => {
+  const request = event.request
+
+  if (request.method !== 'GET') {
+    return
+  }
+
+  const url = new URL(request.url)
+  if (url.origin !== self.location.origin) {
+    return
+  }
+
+  if (url.pathname.startsWith('/github-api')) {
+    return
+  }
+
+  if (request.mode === 'navigate') {
+    event.respondWith(
+      fetch(request)
+        .then((response) => {
+          const copy = response.clone()
+          caches.open(CACHE_NAME).then((cache) => cache.put('/index.html', copy))
+          return response
+        })
+        .catch(() => caches.match('/index.html')),
+    )
+    return
+  }
+
+  event.respondWith(
+    caches.match(request).then((cached) => {
+      if (cached) {
+        return cached
+      }
+
+      return fetch(request)
+        .then((response) => {
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response
+          }
+
+          const copy = response.clone()
+          caches.open(CACHE_NAME).then((cache) => cache.put(request, copy))
+          return response
+        })
+        .catch(() => caches.match('/index.html'))
+    }),
+  )
+})
